3950b142cb084c299aa346ba61ee41e1ebf49f00
jmajurel <jeanfelix.majurel@gmail.com>
2020-04-09 17:51:39 +0200

    added models

---
+++ src/models/address.ts
@@ -0,0 +1,8 @@
+export default class Address {
+  address1: string;
+  address2: string;
+  city: string;
+  postCode: string;
+  state: string;
+  country: string;
+}
---
+++ src/models/doctor.ts
@@ -0,0 +1,5 @@
+export default class Doctor {
+  name: string;
+  address: string;
+  speciality: string;
+}
